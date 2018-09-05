fd1132c3dcf1e0b37d9599e08f5e9d0fcc301f59
Tomasz Krzymianowski <tomasz.krzymianowski@gmail.com>
2018-09-05 18:34:57 +0200

    Change package

--- application/src/main/java/com/krzymianowski/application/CarRentalApplication.java
+++ application/src/main/java/com/krzymianowski/application/CarRentalApplication.java
@@ -1,4 +1,4 @@
-package com.krzymianowski.application.bootloader;
+package com.krzymianowski.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
