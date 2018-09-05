aba2d4230b9d3cc28a1e84aa1bc8946873530827
Tomasz Krzymianowski <tomasz.krzymianowski@gmail.com>
2018-09-05 18:41:02 +0200

    Fix wrong import

--- application/src/main/java/com/krzymianowski/application/model/repository/ColorRepository.java
+++ application/src/main/java/com/krzymianowski/application/model/repository/ColorRepository.java
@@ -1,6 +1,6 @@
 package com.krzymianowski.application.model.repository;
 
-import javafx.scene.paint.Color;
+import com.krzymianowski.application.model.Color;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
