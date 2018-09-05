8eacd22969e518f2f46e9dae1f115feb3ca27cf2
Tomasz Krzymianowski <tomasz.krzymianowski@gmail.com>
2018-09-05 21:07:15 +0200

    Add temp car model for home controller

--- application/src/main/java/com/krzymianowski/application/controller/HomeController.java
+++ application/src/main/java/com/krzymianowski/application/controller/HomeController.java
@@ -1,13 +1,17 @@
 package com.krzymianowski.application.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Arrays;
 
 @Controller
 public class HomeController {
 
     @RequestMapping("/")
-    public String showHomePage(){
+    public String showHomePage(Model model){
+        model.addAttribute("randomCars", Arrays.asList("car1", "car2", "car3", "car4", "car5", "car6", "car7", "car8"));
         return "home-page";
     }
 }
