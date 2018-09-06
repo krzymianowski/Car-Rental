2c8ccad221559c7baf0accf93f6178fa938246c0
Tomasz Krzymianowski <tomasz.krzymianowski@gmail.com>
2018-09-06 18:59:03 +0200

    Create simple storage service

---
+++ application/src/main/java/com/krzymianowski/application/service/StorageService.java
@@ -0,0 +1,16 @@
+package com.krzymianowski.application.service;
+
+import org.springframework.core.io.Resource;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.nio.file.Path;
+
+public interface StorageService {
+
+    void init();
+
+    void store(MultipartFile file);
+
+    void deleteAll();
+
+}
