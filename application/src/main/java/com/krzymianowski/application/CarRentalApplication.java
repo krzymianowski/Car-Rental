ad5ecb8f5ebd5a2f854dc77b415d656298c7a162
Tomasz Krzymianowski <tomasz.krzymianowski@gmail.com>
2018-09-06 19:02:23 +0200

    Add code to init storage service

--- application/src/main/java/com/krzymianowski/application/CarRentalApplication.java
+++ application/src/main/java/com/krzymianowski/application/CarRentalApplication.java
@@ -1,10 +1,27 @@
 package com.krzymianowski.application;
 
+import com.krzymianowski.application.service.StorageService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+
 @SpringBootApplication
 public class CarRentalApplication {
+
+	private final StorageService storageService;
+
+	@Autowired
+	public CarRentalApplication(StorageService storageService) {
+		this.storageService = storageService;
+	}
+
+	@PostConstruct
+	private void initStorage(){
+//		storageService.deleteAll();
+		storageService.init();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CarRentalApplication.class, args);
