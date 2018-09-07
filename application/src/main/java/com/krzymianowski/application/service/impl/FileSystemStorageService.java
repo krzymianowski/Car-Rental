805b78a8bb347e07c4e914247296c33559cdbb2b
Tomasz Krzymianowski <tomasz.krzymianowski@gmail.com>
2018-09-07 07:17:25 +0200

    Add implementation for load resource methods

--- application/src/main/java/com/krzymianowski/application/service/impl/FileSystemStorageService.java
+++ application/src/main/java/com/krzymianowski/application/service/impl/FileSystemStorageService.java
@@ -1,8 +1,10 @@
 package com.krzymianowski.application.service.impl;
 
 import com.krzymianowski.application.exception.StorageException;
+import com.krzymianowski.application.exception.StorageFileNotFoundException;
 import com.krzymianowski.application.service.StorageService;
 import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
 import org.springframework.util.FileSystemUtils;
 import org.springframework.util.StringUtils;
@@ -10,6 +12,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.MalformedURLException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -32,15 +35,15 @@ public class FileSystemStorageService implements StorageService {
     @Override
     public void store(MultipartFile file) {
         String fileName = StringUtils.cleanPath(file.getOriginalFilename());
-        try{
+        try {
             if (file.isEmpty())
                 throw new StorageException("Failed to store empty file: " + fileName);
             if (fileName.contains("..")) //security check
                 throw new StorageException("Cannot store file with relative path outside current directory: " + fileName);
-            try(InputStream inputStream = file.getInputStream()){
+            try (InputStream inputStream = file.getInputStream()) {
                 Files.copy(inputStream, storageLocation.resolve(fileName), StandardCopyOption.REPLACE_EXISTING);
             }
-        } catch (IOException e){
+        } catch (IOException e) {
             throw new StorageException("Failed to store file: " + fileName, e);
         }
     }
@@ -49,5 +52,26 @@ public class FileSystemStorageService implements StorageService {
     public void deleteAll() {
         FileSystemUtils.deleteRecursively(storageLocation.toFile());
     }
+
+    @Override
+    public Path load(String fileName) {
+        return storageLocation.resolve(fileName);
+    }
+
+    @Override
+    public Resource loadAsResource(String fileName) {
+        try{
+            Path file = load(fileName);
+            Resource resource = new UrlResource(file.toUri());
+            if (resource.exists() || resource.isReadable())
+                return resource;
+            else
+                throw new StorageFileNotFoundException("Could not read file: " + fileName);
+
+        } catch (MalformedURLException e) {
+            throw new StorageFileNotFoundException("Could not read file: " + fileName, e);
+        }
+    }
+
 
 }
