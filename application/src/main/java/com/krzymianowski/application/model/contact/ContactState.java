ace5e672f08992952ecc60b7e2f0f8418adfe138
Tomasz Krzymianowski <tomasz.krzymianowski@gmail.com>
2018-09-08 22:02:51 +0200

    Add 'contact state' entity

---
+++ application/src/main/java/com/krzymianowski/application/model/contact/ContactState.java
@@ -0,0 +1,31 @@
+package com.krzymianowski.application.model.contact;
+
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "contact_state")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ContactState {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "contact_state_id")
+    private int id;
+
+    @OneToMany(
+            mappedBy = "state",
+            fetch = FetchType.LAZY)
+    private List<WantToContact> wantToContacts;
+
+    @Column(name = "state_name")
+    private String stateName;
+
+}
