ee270e0f1a45e8480eca0536f7ef4cbece2e8cf2
Tomasz Krzymianowski <tomasz.krzymianowski@gmail.com>
2018-09-08 22:11:04 +0200

    Add validation annotations

--- application/src/main/java/com/krzymianowski/application/model/contact/WantToContact.java
+++ application/src/main/java/com/krzymianowski/application/model/contact/WantToContact.java
@@ -3,6 +3,9 @@ package com.krzymianowski.application.model.contact;
 import lombok.*;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 
 @Entity
@@ -19,18 +22,25 @@ public class WantToContact {
     @Column(name = "want_to_contact_id")
     private int id;
 
+    @NotNull
+    @Size(min = 2, max = 30)
     @Column(name = "first_name")
     private String firstName;
 
+    @Size(max = 30)
     @Column(name = "last_name")
     private String lastName;
 
+    @Email
+    @NotNull
     @Column(name = "email")
     private String email;
 
     @Column(name = "phone")
     private String phone;
 
+    @NotNull
+    @Size(min = 30)
     @Column(name = "message")
     private String message;
 
