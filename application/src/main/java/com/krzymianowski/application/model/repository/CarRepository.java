4bc32131eae56fc89ff8a9c11fc588fd2a337e2b
Tomasz Krzymianowski <tomasz.krzymianowski@gmail.com>
2018-09-08 19:31:20 +0200

    Add 'find all' method for 'home page car' projection

--- application/src/main/java/com/krzymianowski/application/model/repository/CarRepository.java
+++ application/src/main/java/com/krzymianowski/application/model/repository/CarRepository.java
@@ -1,9 +1,14 @@
 package com.krzymianowski.application.model.repository;
 
 import com.krzymianowski.application.model.Car;
+import com.krzymianowski.application.model.projection.HomePageCar;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collection;
+
 @Repository
 public interface CarRepository extends CrudRepository<Car, Integer> {
+
+    Collection<HomePageCar> findAllBy();
 }
