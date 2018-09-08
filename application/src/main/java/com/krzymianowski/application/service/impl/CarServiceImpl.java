50d342b7dae64e023c0663e0714a1459e91d39b1
Tomasz Krzymianowski <tomasz.krzymianowski@gmail.com>
2018-09-08 19:32:01 +0200

    Add right implemetation for 'getHomePageCars' method

--- application/src/main/java/com/krzymianowski/application/service/impl/CarServiceImpl.java
+++ application/src/main/java/com/krzymianowski/application/service/impl/CarServiceImpl.java
@@ -7,7 +7,9 @@ import com.krzymianowski.application.service.CarService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 @Service
 public class CarServiceImpl implements CarService {
@@ -22,6 +24,12 @@ public class CarServiceImpl implements CarService {
 
     @Override
     public List<HomePageCar> getHomePageCars() {
-        return null;
+        List<HomePageCar> allCar = (List<HomePageCar>) carRepository.findAllBy();
+        List<HomePageCar> homePageCars = new ArrayList<>();
+        Random random = new Random();
+
+        for (int i = 0; i < 8; i++)
+            homePageCars.add(allCar.get(random.nextInt(allCar.size())));
+        return homePageCars;
     }
 }
