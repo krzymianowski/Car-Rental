a9b26fbfee859d1bb24793ca1dd431d5fb79fb7a
Tomasz Krzymianowski <tomasz.krzymianowski@gmail.com>
2018-09-08 19:25:58 +0200

    Upgrade temporary home page controller

--- application/src/main/java/com/krzymianowski/application/controller/HomeController.java
+++ application/src/main/java/com/krzymianowski/application/controller/HomeController.java
@@ -1,17 +1,20 @@
 package com.krzymianowski.application.controller;
 
+import com.krzymianowski.application.service.CarService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.Arrays;
-
 @Controller
 public class HomeController {
 
+    @Autowired
+    private CarService carService;
+
     @RequestMapping("/")
-    public String showHomePage(Model model){
-        model.addAttribute("randomCars", Arrays.asList("car1", "car2", "car3", "car4", "car5", "car6", "car7", "car8"));
+    public String showHomePage(Model model) {
+        model.addAttribute("homePageCars", carService.getHomePageCars());
         return "home-page";
     }
 }
