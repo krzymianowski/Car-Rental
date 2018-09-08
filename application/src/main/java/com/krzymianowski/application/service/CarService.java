613e0f1a9a5d0f208166d0a8f0a518f84021e5a1
Tomasz Krzymianowski <tomasz.krzymianowski@gmail.com>
2018-09-08 19:28:33 +0200

    Create getHomePageCar method with empty implementation

--- application/src/main/java/com/krzymianowski/application/service/CarService.java
+++ application/src/main/java/com/krzymianowski/application/service/CarService.java
@@ -1,7 +1,12 @@
 package com.krzymianowski.application.service;
 
 import com.krzymianowski.application.model.Car;
+import com.krzymianowski.application.model.projection.HomePageCar;
+
+import java.util.List;
 
 public interface CarService {
     void save(Car car);
+
+    List<HomePageCar> getHomePageCars();
 }
--- application/src/main/java/com/krzymianowski/application/service/impl/CarServiceImpl.java
+++ application/src/main/java/com/krzymianowski/application/service/impl/CarServiceImpl.java
@@ -1,10 +1,13 @@
 package com.krzymianowski.application.service.impl;
 
 import com.krzymianowski.application.model.Car;
+import com.krzymianowski.application.model.projection.HomePageCar;
 import com.krzymianowski.application.model.repository.CarRepository;
 import com.krzymianowski.application.service.CarService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CarServiceImpl implements CarService {
@@ -15,5 +18,10 @@ public class CarServiceImpl implements CarService {
     @Override
     public void save(Car car) {
         carRepository.save(car);
+    }
+
+    @Override
+    public List<HomePageCar> getHomePageCars() {
+        return null;
     }
 }
