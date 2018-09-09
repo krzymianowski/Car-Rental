7f0786ca4223aeacef214e25e9acb8257fff4154
Tomasz Krzymianowski <tomasz.krzymianowski@gmail.com>
2018-09-09 13:27:31 +0200

    Create empty 'ContactState' service implementation

---
+++ application/src/main/java/com/krzymianowski/application/service/contact/impl/ContactStateServiceImpl.java
@@ -0,0 +1,8 @@
+package com.krzymianowski.application.service.contact.impl;
+
+import com.krzymianowski.application.service.contact.ContactStateService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ContactStateServiceImpl implements ContactStateService {
+}
