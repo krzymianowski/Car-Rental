ee6eca2f68e56a65c12293504a3fdbfc4226b7d3
Tomasz Krzymianowski <tomasz.krzymianowski@gmail.com>
2018-09-09 14:45:17 +0200

    Add getOrCreateState method

--- application/src/main/java/com/krzymianowski/application/service/contact/ContactStateService.java
+++ application/src/main/java/com/krzymianowski/application/service/contact/ContactStateService.java
@@ -1,4 +1,8 @@
 package com.krzymianowski.application.service.contact;
 
+import com.krzymianowski.application.model.contact.ContactState;
+
 public interface ContactStateService {
+
+    ContactState getOrCreateState(String stateName);
 }
--- application/src/main/java/com/krzymianowski/application/service/contact/impl/ContactStateServiceImpl.java
+++ application/src/main/java/com/krzymianowski/application/service/contact/impl/ContactStateServiceImpl.java
@@ -1,8 +1,24 @@
 package com.krzymianowski.application.service.contact.impl;
 
+import com.krzymianowski.application.model.contact.ContactState;
+import com.krzymianowski.application.model.contact.repository.ContactStateRepository;
 import com.krzymianowski.application.service.contact.ContactStateService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ContactStateServiceImpl implements ContactStateService {
+
+    @Autowired
+    private ContactStateRepository contactStateRepository;
+
+    @Override
+    public ContactState getOrCreateState(String stateName) {
+        ContactState contactState = contactStateRepository.getStateByStateName(stateName);
+        if (contactState == null) {
+            contactState = ContactState.builder().stateName(stateName).build();
+            contactStateRepository.save(contactState);
+        }
+        return contactState;
+    }
 }
