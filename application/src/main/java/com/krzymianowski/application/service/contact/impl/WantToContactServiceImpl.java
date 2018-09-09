737db0bd8430455f60f3a095e9bc850cba71925d
Tomasz Krzymianowski <tomasz.krzymianowski@gmail.com>
2018-09-09 13:04:22 +0200

    Create empty implementation for 'WantToContact' service

---
+++ application/src/main/java/com/krzymianowski/application/service/contact/impl/WantToContactServiceImpl.java
@@ -0,0 +1,8 @@
+package com.krzymianowski.application.service.contact.impl;
+
+import com.krzymianowski.application.service.contact.WantToContactService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class WantToContactServiceImpl implements WantToContactService {
+}
