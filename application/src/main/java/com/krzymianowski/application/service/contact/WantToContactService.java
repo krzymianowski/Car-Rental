d20e3234e02bf6e54af62809bb3ecad860b1330d
Tomasz Krzymianowski <tomasz.krzymianowski@gmail.com>
2018-09-09 14:45:46 +0200

    Add save contactForm method

--- application/src/main/java/com/krzymianowski/application/service/contact/WantToContactService.java
+++ application/src/main/java/com/krzymianowski/application/service/contact/WantToContactService.java
@@ -1,4 +1,7 @@
 package com.krzymianowski.application.service.contact;
 
+import com.krzymianowski.application.model.contact.view_model.ContactForm;
+
 public interface WantToContactService {
+    void save(ContactForm contactForm);
 }
--- application/src/main/java/com/krzymianowski/application/service/contact/impl/WantToContactServiceImpl.java
+++ application/src/main/java/com/krzymianowski/application/service/contact/impl/WantToContactServiceImpl.java
@@ -1,8 +1,39 @@
 package com.krzymianowski.application.service.contact.impl;
 
+import com.krzymianowski.application.model.contact.ContactState;
+import com.krzymianowski.application.model.contact.WantToContact;
+import com.krzymianowski.application.model.contact.repository.WantToContactRepository;
+import com.krzymianowski.application.model.contact.view_model.ContactForm;
+import com.krzymianowski.application.service.contact.ContactStateService;
 import com.krzymianowski.application.service.contact.WantToContactService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
 
 @Service
 public class WantToContactServiceImpl implements WantToContactService {
+
+    @Autowired
+    private WantToContactRepository wantToContactRepository;
+
+    @Autowired
+    private ContactStateService contactStateService;
+
+    @Override
+    public void save(ContactForm contactForm) {
+        ContactState contactState = contactStateService.getOrCreateState("Unread");
+
+        WantToContact wantToContact = WantToContact.builder()
+                .firstName(contactForm.getFirstName())
+                .lastName(contactForm.getLastName())
+                .email(contactForm.getEmail())
+                .phone(contactForm.getPhone())
+                .message(contactForm.getMessage())
+                .addDate(new Date())
+                .state(contactState)
+                .build();
+
+        wantToContactRepository.save(wantToContact);
+    }
 }
