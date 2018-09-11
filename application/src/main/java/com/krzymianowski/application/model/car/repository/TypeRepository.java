5ead313bf4519c0dd720650e05f4377ba2b76146
Tomasz Krzymianowski <tomasz.krzymianowski@gmail.com>
2018-09-11 09:55:58 +0200

    Add repository method for getting all types

--- application/src/main/java/com/krzymianowski/application/model/car/repository/TypeRepository.java
+++ application/src/main/java/com/krzymianowski/application/model/car/repository/TypeRepository.java
@@ -1,11 +1,21 @@
 package com.krzymianowski.application.model.car.repository;
 
 import com.krzymianowski.application.model.car.Type;
+import com.krzymianowski.application.model.car.projection.OurCarsPageType;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.Collection;
+import java.util.List;
 
 @Repository
 public interface TypeRepository extends CrudRepository<Type, Integer> {
 
     Type getTypeByTypeName(String typeName);
+
+    <T> Collection<T> findAllBy(Class<T> tClass);
+
+    default <T> Collection<T>  getAll(Class<T> tClass){
+        return findAllBy(tClass);
+    }
 }
