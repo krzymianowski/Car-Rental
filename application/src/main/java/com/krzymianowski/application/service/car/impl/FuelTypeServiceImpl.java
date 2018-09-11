a4b509310024cc68ae5fb4e451df192498cf2549
Tomasz Krzymianowski <tomasz.krzymianowski@gmail.com>
2018-09-11 12:10:27 +0200

    Add implementation for getOurCarsPageFuelTypes method

--- application/src/main/java/com/krzymianowski/application/service/car/impl/FuelTypeServiceImpl.java
+++ application/src/main/java/com/krzymianowski/application/service/car/impl/FuelTypeServiceImpl.java
@@ -1,10 +1,13 @@
 package com.krzymianowski.application.service.car.impl;
 
 import com.krzymianowski.application.model.car.FuelType;
+import com.krzymianowski.application.model.car.projection.OurCarsPageFuelType;
 import com.krzymianowski.application.model.car.repository.FuelTypeRepository;
 import com.krzymianowski.application.service.car.FuelTypeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class FuelTypeServiceImpl implements FuelTypeService {
@@ -20,5 +23,10 @@ public class FuelTypeServiceImpl implements FuelTypeService {
     @Override
     public void save(FuelType fuelType) {
         fuelTypeRepository.save(fuelType);
+    }
+
+    @Override
+    public List<OurCarsPageFuelType> getOurCarsPageFuelTypes() {
+        return (List<OurCarsPageFuelType>) fuelTypeRepository.getAll(OurCarsPageFuelType.class);
     }
 }
