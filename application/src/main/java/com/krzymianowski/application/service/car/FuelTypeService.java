dd6a99eb2b576ebdd0535a6298dec55f61c6d9be
Tomasz Krzymianowski <tomasz.krzymianowski@gmail.com>
2018-09-11 12:10:07 +0200

    Add getOurCarsPageFuelTypes method

--- application/src/main/java/com/krzymianowski/application/service/car/FuelTypeService.java
+++ application/src/main/java/com/krzymianowski/application/service/car/FuelTypeService.java
@@ -1,9 +1,14 @@
 package com.krzymianowski.application.service.car;
 
 import com.krzymianowski.application.model.car.FuelType;
+import com.krzymianowski.application.model.car.projection.OurCarsPageFuelType;
+
+import java.util.List;
 
 public interface FuelTypeService {
     FuelType getFuelTypeByTypeName(String fuelTypeName);
 
     void save(FuelType fuelType);
+
+    List<OurCarsPageFuelType> getOurCarsPageFuelTypes();
 }
