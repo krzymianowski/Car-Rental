38b04848ae6eef28bdace9e4110b9942d3dcac30
Tomasz Krzymianowski <tomasz.krzymianowski@gmail.com>
2018-09-11 10:56:31 +0200

    Add getAll method

--- application/src/main/java/com/krzymianowski/application/model/car/repository/BrandRepository.java
+++ application/src/main/java/com/krzymianowski/application/model/car/repository/BrandRepository.java
@@ -4,8 +4,16 @@ import com.krzymianowski.application.model.car.Brand;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collection;
+
 @Repository
 public interface BrandRepository extends CrudRepository<Brand, Integer> {
 
     Brand getBrandByBrandName(String brand_name);
+
+    <T> Collection<T> findAllBy(Class<T> tClass);
+
+    default <T> Collection<T>  getAll(Class<T> tClass){
+        return findAllBy(tClass);
+    }
 }
