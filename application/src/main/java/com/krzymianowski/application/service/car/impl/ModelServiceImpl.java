3ea8e92a5d19df08f6813fd0688c6f0553e253c0
Tomasz Krzymianowski <tomasz.krzymianowski@gmail.com>
2018-09-11 11:22:39 +0200

    Add implementation for getOurCarsPageModels method

--- application/src/main/java/com/krzymianowski/application/service/car/impl/ModelServiceImpl.java
+++ application/src/main/java/com/krzymianowski/application/service/car/impl/ModelServiceImpl.java
@@ -2,10 +2,13 @@ package com.krzymianowski.application.service.car.impl;
 
 import com.krzymianowski.application.model.car.Brand;
 import com.krzymianowski.application.model.car.Model;
+import com.krzymianowski.application.model.car.projection.OurCarsPageModel;
 import com.krzymianowski.application.model.car.repository.ModelRepository;
 import com.krzymianowski.application.service.car.ModelService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ModelServiceImpl implements ModelService {
@@ -21,5 +24,10 @@ public class ModelServiceImpl implements ModelService {
     @Override
     public void save(Model model) {
         modelRepository.save(model);
+    }
+
+    @Override
+    public List<OurCarsPageModel> getOurCarsPageModels(String brandName) {
+        return (List<OurCarsPageModel>) modelRepository.getBrandName(brandName, OurCarsPageModel.class);
     }
 }
