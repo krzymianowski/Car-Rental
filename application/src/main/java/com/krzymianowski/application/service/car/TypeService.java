bdc1c5ee4c5f3e91faba2ea29f31a8fb549825db
Tomasz Krzymianowski <tomasz.krzymianowski@gmail.com>
2018-09-11 09:54:04 +0200

    Add getOurCarsPageTypes method

--- application/src/main/java/com/krzymianowski/application/service/car/TypeService.java
+++ application/src/main/java/com/krzymianowski/application/service/car/TypeService.java
@@ -1,9 +1,14 @@
 package com.krzymianowski.application.service.car;
 
 import com.krzymianowski.application.model.car.Type;
+import com.krzymianowski.application.model.car.projection.OurCarsPageType;
+
+import java.util.List;
 
 public interface TypeService {
     Type getTypeByTypeName(String typeName);
 
     void save(Type type);
+
+    List<OurCarsPageType> getOurCarsPageTypes();
 }
