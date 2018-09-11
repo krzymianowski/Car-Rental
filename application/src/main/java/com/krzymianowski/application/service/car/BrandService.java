1bda6d853e2ae41fafc174a190ae2a121e129ea1
Tomasz Krzymianowski <tomasz.krzymianowski@gmail.com>
2018-09-11 10:55:35 +0200

    Add getOurCarsPageBrand method

--- application/src/main/java/com/krzymianowski/application/service/car/BrandService.java
+++ application/src/main/java/com/krzymianowski/application/service/car/BrandService.java
@@ -1,9 +1,14 @@
 package com.krzymianowski.application.service.car;
 
 import com.krzymianowski.application.model.car.Brand;
+import com.krzymianowski.application.model.car.projection.OurCarsPageBrand;
+
+import java.util.List;
 
 public interface BrandService {
     Brand getBrandByBrandName(String brand_name);
 
     void save(Brand brand);
+
+    List<OurCarsPageBrand> getOurCarsPageBrands();
 }
