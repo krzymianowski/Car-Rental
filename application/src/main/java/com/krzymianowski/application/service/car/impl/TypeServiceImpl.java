87645476d25afb944dd28978ae5c01a83547df6c
Tomasz Krzymianowski <tomasz.krzymianowski@gmail.com>
2018-09-11 09:54:40 +0200

    Add implementation for getOurCarsPageTypes method

--- application/src/main/java/com/krzymianowski/application/service/car/impl/TypeServiceImpl.java
+++ application/src/main/java/com/krzymianowski/application/service/car/impl/TypeServiceImpl.java
@@ -1,10 +1,14 @@
 package com.krzymianowski.application.service.car.impl;
 
 import com.krzymianowski.application.model.car.Type;
+import com.krzymianowski.application.model.car.projection.OurCarsPageCar;
+import com.krzymianowski.application.model.car.projection.OurCarsPageType;
 import com.krzymianowski.application.model.car.repository.TypeRepository;
 import com.krzymianowski.application.service.car.TypeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class TypeServiceImpl implements TypeService {
@@ -20,5 +24,10 @@ public class TypeServiceImpl implements TypeService {
     @Override
     public void save(Type type) {
         typeRepository.save(type);
+    }
+
+    @Override
+    public List<OurCarsPageType> getOurCarsPageTypes() {
+        return (List<OurCarsPageType>) typeRepository.getAll(OurCarsPageType.class);
     }
 }
