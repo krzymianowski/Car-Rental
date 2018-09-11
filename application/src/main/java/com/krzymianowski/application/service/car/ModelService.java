2b1efc6cdd78e3c9b5809a96e0f515544daaf2ae
Tomasz Krzymianowski <tomasz.krzymianowski@gmail.com>
2018-09-11 11:22:16 +0200

    Add getOurCarsPageModels method

--- application/src/main/java/com/krzymianowski/application/service/car/ModelService.java
+++ application/src/main/java/com/krzymianowski/application/service/car/ModelService.java
@@ -2,9 +2,14 @@ package com.krzymianowski.application.service.car;
 
 import com.krzymianowski.application.model.car.Brand;
 import com.krzymianowski.application.model.car.Model;
+import com.krzymianowski.application.model.car.projection.OurCarsPageModel;
+
+import java.util.List;
 
 public interface ModelService {
     Model getModelByBrandAndModelName(Brand brand, String modelName);
 
     void save(Model model);
+
+    List<OurCarsPageModel> getOurCarsPageModels(String brandName);
 }
