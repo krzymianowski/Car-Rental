6d8d7938ef128b6eeb19450035ba234bca6f212b
Tomasz Krzymianowski <tomasz.krzymianowski@gmail.com>
2018-09-11 12:10:46 +0200

    Add getAll method

--- application/src/main/java/com/krzymianowski/application/model/car/repository/FuelTypeRepository.java
+++ application/src/main/java/com/krzymianowski/application/model/car/repository/FuelTypeRepository.java
@@ -1,11 +1,20 @@
 package com.krzymianowski.application.model.car.repository;
 
 import com.krzymianowski.application.model.car.FuelType;
+import com.krzymianowski.application.model.car.projection.OurCarsPageFuelType;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.Collection;
 
 @Repository
 public interface FuelTypeRepository extends CrudRepository<FuelType, Integer> {
 
     FuelType getFuelTypeByTypeName(String fuelTypeName);
+
+    <T> Collection<T> findAllBy(Class<T> tClass);
+
+    default <T> Collection<T> getAll(Class<T> tClass) {
+        return findAllBy(tClass);
+    }
 }
