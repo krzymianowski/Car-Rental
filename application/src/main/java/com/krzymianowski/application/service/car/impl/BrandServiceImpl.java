648bcdd4bb86a6ee83fff20790372bf7919a4f1e
Tomasz Krzymianowski <tomasz.krzymianowski@gmail.com>
2018-09-11 10:56:05 +0200

    Add implementation for getOurCarsPageBrand method

--- application/src/main/java/com/krzymianowski/application/service/car/impl/BrandServiceImpl.java
+++ application/src/main/java/com/krzymianowski/application/service/car/impl/BrandServiceImpl.java
@@ -1,10 +1,13 @@
 package com.krzymianowski.application.service.car.impl;
 
 import com.krzymianowski.application.model.car.Brand;
+import com.krzymianowski.application.model.car.projection.OurCarsPageBrand;
 import com.krzymianowski.application.model.car.repository.BrandRepository;
 import com.krzymianowski.application.service.car.BrandService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class BrandServiceImpl implements BrandService {
@@ -20,5 +23,10 @@ public class BrandServiceImpl implements BrandService {
     @Override
     public void save(Brand brand) {
         brandRepository.save(brand);
+    }
+
+    @Override
+    public List<OurCarsPageBrand> getOurCarsPageBrands() {
+        return (List<OurCarsPageBrand>) brandRepository.getAll(OurCarsPageBrand.class);
     }
 }
