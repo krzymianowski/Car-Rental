cb05d4815c419b6ded6c0496408e7b6462a3d4b7
Tomasz Krzymianowski <tomasz.krzymianowski@gmail.com>
2018-09-13 15:35:22 +0200

    Add car details to model

--- application/src/main/java/com/krzymianowski/application/controller/CarDetailsController.java
+++ application/src/main/java/com/krzymianowski/application/controller/CarDetailsController.java
@@ -1,14 +1,21 @@
 package com.krzymianowski.application.controller;
 
+import com.krzymianowski.application.service.car.CarService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class CarDetailsController {
 
+    @Autowired
+    private CarService carService;
+
     @RequestMapping("/car-details/{carId}")
-    public String showCarDetailsPage(@PathVariable("carId") int carId) {
+    public String showCarDetailsPage(@PathVariable("carId") int carId, Model model) {
+        model.addAttribute(carService.getCarDetails(carId));
         return "car-details";
     }
 }
