5524f9c9eefc1a877132c554f45574d319976abd
Tomasz Krzymianowski <tomasz.krzymianowski@gmail.com>
2018-09-13 12:25:21 +0200

    Change table name and id column name

--- application/src/main/java/com/krzymianowski/application/model/car/Rating.java
+++ application/src/main/java/com/krzymianowski/application/model/car/Rating.java
@@ -5,7 +5,7 @@ import lombok.*;
 import javax.persistence.*;
 
 @Entity
-@Table(name = "rating")
+@Table(name = "car_rating")
 @Getter
 @Setter
 @NoArgsConstructor
@@ -15,7 +15,7 @@ public class Rating {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "rating_id")
+    @Column(name = "car_rating_id")
     private int id;
 
     @Column(name = "economy")
