ffb0d1ec5de0b68dea4ae73cfccf5a45d4277192
Tomasz Krzymianowski <tomasz.krzymianowski@gmail.com>
2018-09-14 18:32:59 +0200

    Create subscriber entity

---
+++ application/src/main/java/com/krzymianowski/application/model/newsletter/Subscriber.java
@@ -0,0 +1,30 @@
+package com.krzymianowski.application.model.newsletter;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "subscriber", uniqueConstraints = {@UniqueConstraint(columnNames = {"email"})})
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Subscriber {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "subscriber_id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "enabled")
+    private boolean isEnabled = true;
+
+}
