50a0bb0434e7031c90729e8c42deb53294557456
Tomasz Krzymianowski <tomasz.krzymianowski@gmail.com>
2018-09-14 18:34:20 +0200

    Create subscriber service

---
+++ application/src/main/java/com/krzymianowski/application/service/newletter/SubscriberService.java
@@ -0,0 +1,7 @@
+package com.krzymianowski.application.service.newletter;
+
+import com.krzymianowski.application.model.newsletter.view_model.NewsletterForm;
+
+public interface SubscriberService {
+    void save(NewsletterForm newsletterForm);
+}
