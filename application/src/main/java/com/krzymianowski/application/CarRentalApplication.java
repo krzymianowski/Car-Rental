21b5078324d717a033d47fc652568e73aff181c5
Tomasz Krzymianowski <tomasz.krzymianowski@gmail.com>
2018-09-18 20:36:07 +0200

    Reformat code

--- application/src/main/java/com/krzymianowski/application/CarRentalApplication.java
+++ application/src/main/java/com/krzymianowski/application/CarRentalApplication.java
@@ -3,25 +3,27 @@ package com.krzymianowski.application;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 import javax.annotation.PostConstruct;
 
 @SpringBootApplication
-public class CarRentalApplication {
+public class CarRentalApplication extends SpringBootServletInitializer{
 
-    private final InitTempData initTempData;
-
-    @Autowired
-    public CarRentalApplication(InitTempData initTempData) {
-        this.initTempData = initTempData;
-    }
+//    private final InitTempData initTempData;
+//
+//    @Autowired
+//    public CarRentalApplication(InitTempData initTempData) {
+//        this.initTempData = initTempData;
+//    }
+//
+//
+//    @PostConstruct
+//    private void initData() {
+//        initTempData.initDatabaseItems();
+//    }
 
     public static void main(String[] args) {
         SpringApplication.run(CarRentalApplication.class, args);
-    }
-
-    @PostConstruct
-    private void initData() {
-//        initTempData.initDatabaseItems();
     }
 }
