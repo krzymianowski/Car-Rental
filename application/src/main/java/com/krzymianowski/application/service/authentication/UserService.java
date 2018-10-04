ab09af0b7289e44bc98a71d5a45a5668b1e6df21
Tomasz Krzymianowski <tomasz.krzymianowski@gmail.com>
2018-10-04 20:17:43 +0200

    Add user service with addUser method

---
+++ application/src/main/java/com/krzymianowski/application/service/authentication/UserService.java
@@ -0,0 +1,7 @@
+package com.krzymianowski.application.service.authentication;
+
+import com.krzymianowski.application.model.authentication.User;
+
+public interface UserService {
+    void addUser(User user);
+}
