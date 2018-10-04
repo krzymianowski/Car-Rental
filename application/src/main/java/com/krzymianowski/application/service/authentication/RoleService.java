ea90404de8714e2030685e90170fc21668c1a728
Tomasz Krzymianowski <tomasz.krzymianowski@gmail.com>
2018-10-04 20:17:02 +0200

    Add role service with addRole method

---
+++ application/src/main/java/com/krzymianowski/application/service/authentication/RoleService.java
@@ -0,0 +1,8 @@
+package com.krzymianowski.application.service.authentication;
+
+import com.krzymianowski.application.model.authentication.Role;
+
+public interface RoleService {
+
+    void addRole(Role role);
+}
