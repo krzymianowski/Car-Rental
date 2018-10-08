dac3ec918c8367caa2ab9b81ee9244c514ebcc17
Tomasz Krzymianowski <tomasz.krzymianowski@gmail.com>
2018-10-08 16:41:09 +0200

    Add control-panel/add-car get mapping

--- application/src/main/java/com/krzymianowski/application/controller/ControlPanelController.java
+++ application/src/main/java/com/krzymianowski/application/controller/ControlPanelController.java
@@ -12,4 +12,9 @@ public class ControlPanelController {
     public String showControlPanel() {
         return "control-panel";
     }
+
+    @GetMapping("/add-car")
+    public String showAddCarPage() {
+        return "control-panel_add-car";
+    }
 }
